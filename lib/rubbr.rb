09404e5303f0da5de1c536f1d0e82ecde477affd
oleg dashevskii <olegdashevskii@gmail.com>
2013-06-03 19:07:32 +0700

    Version 1.1.6

--- lib/rubbr.rb
+++ lib/rubbr.rb
@@ -2,7 +2,7 @@ require 'optparse'
 $:.unshift File.dirname(__FILE__)
 
 module Rubbr
-  VERSION = '1.1.5.1'
+  VERSION = '1.1.6'
 
   autoload :Options,       'rubbr/options'
   autoload :OS,            'rubbr/os'
